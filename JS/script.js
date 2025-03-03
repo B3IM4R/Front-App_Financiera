52f918b6b1c77ceb7b74043d3e6b1f742abed851
B3IM4R <beimar.mestizo@gmail.com>
2025-03-03 03:02:16 -0500

    add the responsive design for the main container, the aside and the footer with some styles

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,10 @@
+const links = document.querySelectorAll('#menu-lateral a');
+const sections = document.querySelectorAll('section');
+const menuToggle = document.getElementById('menu-toggle');
+const aside = document.getElementById('menu-lateral');
+const openModal = document.getElementById('open-modal');
+const modal = document.getElementById('modal');
+const closeModal = document.getElementById('close-modal');
 const instrucciones = [
     {
         titulo: "Organiza tus Datos",
@@ -23,10 +30,58 @@ function generarInstrucciones(contenedor) {
     contenedor.innerHTML = html;
 }
 
-// Generar contenido en el footer y modal
 document.addEventListener("DOMContentLoaded", () => {
     const footerInstrucciones = document.getElementById('footer-instrucciones');
     if (footerInstrucciones) {
         generarInstrucciones(footerInstrucciones);
     }
+
+    const modalInstrucciones = document.getElementById('modal-instrucciones');
+    if (modalInstrucciones) {
+        generarInstrucciones(modalInstrucciones);
+    }
+});
+
+function toggleMenu() {
+    aside.classList.toggle('active');
+    document.body.classList.toggle('menu-activo');
+
+    if (aside.classList.contains('active')) {
+        menuToggle.textContent = '✕';
+    } else {
+        menuToggle.textContent = '☰';
+    }
+}
+
+menuToggle.addEventListener('click', toggleMenu);
+
+openModal.addEventListener('click', (event) => {
+    openModal.style.color = '#007BFF';
+    event.preventDefault();
+    modal.style.display = 'block';
+});
+
+closeModal.addEventListener('click', () => {
+    openModal.style.color = '';
+    modal.style.display = 'none';
+});
+
+window.addEventListener('scroll', () => {
+    let current = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if (pageYOffset >= sectionTop - sectionHeight / 3) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    links.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href') === `#${current}`) {
+            link.classList.add('active');
+        }
+    });
 });
